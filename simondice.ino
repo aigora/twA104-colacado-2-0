aaac17965cce0bdcbd236708c687bce2dfb359da
JPioGA <47852936+JPioGA@users.noreply.github.com>
2019-04-22 12:46:45 +0200

    S-D cambio 2
    En este cambio:
    1º He creado la función leer_secuencia
    2º He realizado retoques en algunas funciones
    
    OBJETIVOS PENDIENTES (siguientes cambios):
    - Crear el vector "secuencia" de forma aleatoria y con asignación dinámica de memoria.
    - Programar en el loop el flujo del programa(con las comparaciones, salidas por pantalla...).
    - Crear el mismo programa con el mayor número posible de punteros.
    - Implementar la salida de información por pantallla LCD para interactuar con el usuario.
    - Introducir salida y entrada de informacion mediante ficheros.(High scores, historial de juegos...). (Creo que esto no se puede hacer con arduino.)
    
    OBJETIVOS ALCANZADOS:
    X Crear función para la lectura de las entradas de los botones y creación de un vector a partir de ellas. (HECHO en cambio 2)

--- simondice.ino
+++ simondice.ino
@@ -3,16 +3,19 @@
 void inicio_juego();        //La función "inicio_juego" muestra que se ha iniciado el juego.
 void game_over();        //La función "game_over" muestra que se has perdido el juego.
 void fin_secuencia();       //La función "fin_secuencia" enciende y apaga 2 veces los leds para indicar el fin de la secuencia mostrada por los leds.
-void enunciado(int, int[]); //La función "enunciado" mostrará por los 4 leds la secuencia creada aleatoriamente.
+void enunciado_secuencia(int, int[]);     //La función "enunciado_secuencia" mostrará por los 4 leds la secuencia creada aleatoriamente.
+void leer_secuencia(int);                 //La función "leer_secuencia" se encargará de leer la secuencia  introducida por el usuario mediante los botones.
+void mostrar_fallos();                    //La función "mostrar_fallo" te dirá en que botón has fallado.
 
 
 //Variables
 int led[4]={2, 3, 4, 5};
 int boton[4]={10, 11, 12, 13};
-int i, ronda=8, secuencia[8]={0, 1, 2, 3, 3, 0, 2, 1};
+int i, ronda=8, secuencia[8]={0, 1, 2, 3, 3, 0, 2, 1}, pulso[8], flagfail=0;
     // "ronda" indica en que nivel estamos y el número de leds que se encenderán.
     // "secuencia" es un vector creado aleatoriamente que contendrá la posicón en el vector del led que se tiene que encender.
-
+    // "pulso" es un vector donde guardaré los botones que pulsará el usuario en su turno de juego para después compararlo con la secuencia.
+    // "flagfail" me servirá para saber si hay algún fallo en la secuencia introducida por los botones.
     
 void setup() {
   //Inicializo los pines de entrada y salida
@@ -26,15 +29,29 @@ void setup() {
 
 void loop() {
   inicio_juego();
-  
-  enunciado(ronda, secuencia);
-  delay(3000);
+
+//  while(ronda<=8 && flagfail==0) //Bucle en el que se dearrolla el juego. Saldré de el en caso de fallar (flagfail=1) o en caso de que se me acabe el vector secuencia.
+//  {
+    enunciado_secuencia(ronda, secuencia);
+    delay(3000);
+    leer_secuencia(ronda);
+    delay(3000);
     
+//    for(i=0;i<ronda;i++) //Comprobación de si he acertado o no en la ronda. (Comparo si los vectores de secuencia y pulso son iguales).
+//    {
+//      if(secuencia[i] != pulso[i]) //En caso de fallar, activo el flagfail a 1 
+//      {
+//        flagfail=1;
+//      }
+//    }
+  
   game_over();
 }
 
 
-// Funciones.
+
+
+// FUNCIONES.
 
 void enunciado(int x, int vect[])  // "x" es la ronda. "vect[]" es el vector aleatorio con los leds a encender, es decir la secuencia.
 {
@@ -50,8 +67,74 @@ void enunciado(int x, int vect[])  // "x" es la ronda. "vect[]" es el vector ale
     digitalWrite(luz, LOW);
     delay(250);
   }
+  
+  for(i=0;i<ronda;i++) //Muestro los botones pulsados (test de comprobación)
+  {
+  Serial.print(secuencia[i]);
+  }
+    
   fin_secuencia();
 }
+
+
+
+void leer_secuencia(int x) // "x" es la ronda. "pulso[]" es el vector que iré creando a medida que vaya pulsando
+{
+  int i;
+
+  
+  for(i=0;i<x;i++) // "x" es la ronda. "pulso[]" es el vector donde se guarda las pulsaciones del usuario. 
+  {
+    int i;
+    Serial.print("Introduce pulso ");
+    Serial.println(i);
+    // ¡¡¡OJO!!! Para que se rellene el vector pulso hay que mantener pulsado el boton hasta que parpadee la luz (confirmación de lectura).
+    
+    digitalRead(10);
+    digitalRead(11);
+    digitalRead(12);
+    digitalRead(13);
+    delay(3000);
+    
+    if(digitalRead(10)==HIGH)
+    {
+      pulso[i]=0;
+      digitalWrite(2, HIGH); //CONFIRMACIÓN DE LECTURA.
+      delay(100);
+      digitalWrite(2, LOW);
+    }
+    else if(digitalRead(11)==HIGH)
+    {
+      pulso[i]=1;
+      digitalWrite(3, HIGH);
+      delay(100);
+      digitalWrite(3, LOW);
+    }
+    else if(digitalRead(12)==HIGH)
+    {
+      pulso[i]=2;
+      digitalWrite(4, HIGH);
+      delay(100);
+      digitalWrite(4, LOW);
+    }
+    else if(digitalRead(13)==HIGH)
+    {
+      pulso[i]=3;
+      digitalWrite(5, HIGH);
+      delay(100);
+      digitalWrite(5, LOW);
+    }
+    delay(1000);
+  }
+  
+  for(i=0;i<ronda;i++) //Muestro los botones pulsados
+  {
+  Serial.print(pulso[i]);
+  }
+  Serial.println("\n");
+}
+
+
 
 void fin_secuencia()
 {
@@ -77,11 +160,17 @@ void fin_secuencia()
   delay(75);
   Serial.println("Fin de secuencia.");
 }
+
+
+
 void inicio_juego()
 {
-  Serial.println("INICIO SIMÓN DICE.");
+  Serial.println("\nINICIO SIMÓN DICE.");
 }
+
+
+
 void game_over()
 {
-  Serial.println("GAME OVER SIMÓN DICE.\n");
+  Serial.println("\nGAME OVER SIMÓN DICE.\n");
 }
